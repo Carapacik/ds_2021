4f5346e901c8aa6104dc00733b333b017c35c40f
Roman <carapacik@gmail.com>
2021-04-13 17:21:33 +0300

    Удаление старых файлов

--- Valuator/Pages/About.cshtml.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace Valuator.Pages
-{
-    public class About : PageModel
-    {
-    }
-}
--- scripts/dockerstop.cmd
+++
@@ -1,5 +0,0 @@
-docker stop valuatorRedis & docker rm valuatorRedis
-docker stop valuatorRedisRU & docker rm valuatorRedisRU
-docker stop valuatorRedisEU & docker rm valuatorRedisEU
-docker stop valuatorRedisOther & docker rm valuatorRedisOther
-docker stop valuatorNats & docker rm valuatorNats
